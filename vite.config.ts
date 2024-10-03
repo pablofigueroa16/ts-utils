d1954403f23e10a247f09f460456f2d25fb3ceb3
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-10-03 07:04:55 -0300

    initialize library

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve} from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'ts-utils',
+        },
+        // mapa para debugear la libreria y contiene la informacion del codigo de la libreria antes de ser transpilado
+        sourcemap: true,
+        target: 'es6',
+        minify: false,
+    },
+    plugins: [dts({outDir: 'dist'})],
+}) 
