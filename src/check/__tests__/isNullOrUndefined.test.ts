25d7f6962cb3680173539481308ad19e9e457d0a
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-11-11 22:30:28 -0300

    chore: kill all mutation on isNullOrUndefined test

--- src/check/__tests__/isNullOrUndefined.test.ts
+++ src/check/__tests__/isNullOrUndefined.test.ts
@@ -6,6 +6,7 @@ describe("isNullOrUndefined", () => {
 	it.each([
 		{ value: null, results: true },
 		{ value: undefined, results: true },
+		{ value: true, results: false },
 	])("should return $results if the value is $value", ({ value, results }) => {
 		expect(isNullOrUndefined(value)).toBe(results);
 	});
