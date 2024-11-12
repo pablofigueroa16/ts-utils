1d6bb922b4b689d92aabd483384df11018581318
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-11-11 23:13:00 -0300

    chore: add tests

--- src/check/__tests__/isObject.test.ts
+++ src/check/__tests__/isObject.test.ts
@@ -9,4 +9,19 @@ describe("isObject", () => {
 	it("should return false if the value is not an object", () => {
 		expect(isObject(10)).toBe(false);
 	});
+	it("should return false if the value is true", () => {
+		expect(isObject(true)).toBe(false);
+	});
+	it("should return false if the value is false", () => {
+		expect(isObject(false)).toBe(false);
+	});
+	it("should return false if the value is null", () => {
+		expect(isObject(null)).toBe(false);
+	});
+	it("should return false if the value is undefined", () => {
+		expect(isObject(undefined)).toBe(false);
+	});
+	it("should return false if the value is a function", () => {
+		expect(isObject(() => {})).toBe(false);
+	});
 });
--- src/timing/__tests__/debounce.test.ts
+++ src/timing/__tests__/debounce.test.ts
@@ -25,4 +25,87 @@ describe("debounce", () => {
 		debouncedFn();
 		expect(fn).toHaveBeenCalledTimes(1);
 	});
+	it("should no debounce the function if the delay is undefined", () => {
+		const fn = vi.fn();
+		const debouncedFn = debounce(fn, undefined);
+		debouncedFn();
+		debouncedFn();
+		vi.advanceTimersByTime(100);
+		expect(fn).toHaveBeenCalledTimes(0);
+	});
+	it("should debounce the function and no execute it after the default delay", () => {
+		const fn = vi.fn();
+		const debouncedFn = debounce(fn);
+		debouncedFn();
+		debouncedFn();
+		vi.advanceTimersByTime(200);
+		debouncedFn();
+		expect(fn).toHaveBeenCalledTimes(1);
+	});
+	it("should execute the function only once if called multiple times within delay", () => {
+		const fn = vi.fn();
+		const debouncedFn = debounce(fn, 100);
+
+		debouncedFn();
+		debouncedFn();
+		vi.advanceTimersByTime(100);
+
+		expect(fn).toHaveBeenCalledTimes(1);
+	});
+
+	it("should not execute the function if not called again after clearing timeout", () => {
+		const fn = vi.fn();
+		const debouncedFn = debounce(fn, 100);
+
+		debouncedFn();
+		vi.advanceTimersByTime(50);
+		debouncedFn();
+
+		vi.advanceTimersByTime(50);
+
+		expect(fn).toHaveBeenCalledTimes(0);
+
+		vi.advanceTimersByTime(100);
+		expect(fn).toHaveBeenCalledTimes(1);
+	});
+
+	it("should not call clearTimeout if debounce is called once and delay time has passed", () => {
+		const fn = vi.fn();
+		const debouncedFn = debounce(fn, 100);
+
+		debouncedFn();
+		vi.advanceTimersByTime(100);
+
+		expect(fn).toHaveBeenCalledTimes(1);
+
+		debouncedFn();
+		vi.advanceTimersByTime(100);
+		expect(fn).toHaveBeenCalledTimes(2);
+	});
+
+	it("should only call clearTimeout when debounce is called again within delay", () => {
+		const fn = vi.fn();
+		const debouncedFn = debounce(fn, 100);
+
+		debouncedFn();
+		vi.advanceTimersByTime(50);
+
+		debouncedFn();
+		expect(fn).toHaveBeenCalledTimes(0);
+
+		vi.advanceTimersByTime(100);
+		expect(fn).toHaveBeenCalledTimes(1);
+	});
+	it("should call clearTimeout when debounce is called multiple times", () => {
+		const fn = vi.fn();
+		const debouncedFn = debounce(fn, 100);
+
+		debouncedFn();
+		vi.advanceTimersByTime(50);
+
+		expect(fn).not.toHaveBeenCalled();
+
+		vi.advanceTimersByTime(50);
+		expect(fn).toHaveBeenCalledTimes(1);
+	});
 });
