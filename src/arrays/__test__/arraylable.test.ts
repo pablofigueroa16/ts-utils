a035ba0264c42521d0beea9d6d1c43b6aa2468db
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-12-05 22:18:12 -0300

    feat: add test for arraylable file

---
+++ src/arrays/__test__/arraylable.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from "vitest";
+import { arraylable } from "./../arraylable";
+
+describe("arraylable", () => {
+	it("debería devolver el arreglo cuando se pasa un arreglo válido", () => {
+		const input = [1, 2, 3];
+		const result = arraylable(input);
+		expect(result).toEqual(input);
+	});
+
+	it("debería devolver un arreglo vacío cuando se pasa null", () => {
+		const result = arraylable(null);
+		expect(result).toEqual([]);
+	});
+
+	it("debería devolver un arreglo vacío cuando se pasa un valor que no es un arreglo", () => {
+		// @ts-expect-error necesary to test the function
+		const result = arraylable("no soy un arreglo");
+		expect(result).toEqual([]);
+	});
+
+	it("debería devolver un arreglo vacío cuando se pasa un objeto", () => {
+		// @ts-expect-error necesary to test the function
+		const result = arraylable({ key: "value" });
+		expect(result).toEqual([]);
+	});
+
+	it("debería devolver un arreglo vacío cuando se pasa undefined", () => {
+		// @ts-expect-error necesary to test the function
+		const result = arraylable(undefined);
+		expect(result).toEqual([]);
+	});
+});
